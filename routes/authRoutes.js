a13f02a8d4605872f7df445613be229ac51624c5
MATAN\matan <matany90@gmail.com>
2019-12-11 22:28:50 +0200

    refactor

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,7 +8,7 @@ module.exports = (app) => {
     }))
 
     app.get('/auth/google/callback',
-        passport.authenticate('google', {session: false}),
+        passport.authenticate('google', {session: false, failureRedirect: '/'}),
         (req, res) => {
             res.send('test')
            // res.redirect('https://gentle-shelf-52680.herokuapp.com/categories');
