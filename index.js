f70a763b96dee92410ea907822c775942c876abe
MATAN\matan <matany90@gmail.com>
2019-12-11 23:05:53 +0200

    refactor

--- index.js
+++ index.js
@@ -33,5 +33,18 @@ app.use(bodyParser.json()); //make POST req include the body to req obj
 
 require('./routes/authRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+    /* Express will serve up production assets
+     like out main.js file, or main.css file! */
+    app.use(express.static('./client/build'));
+    /* Express will serve up the index.html file
+    if it doesnt recognize the route */
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+
+}
+
 const PORT = process.env.PORT || 5000; // listen to Heroku's given port (prod), or take 5000 (dev)
 app.listen(PORT);
