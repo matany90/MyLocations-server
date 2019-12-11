5ced86ce413d925691888cef7c9fa9af0757c5df
MATAN\matan <matany90@gmail.com>
2019-12-11 16:35:43 +0200

    express and mongoose buildup

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 const express = require('express');
 const cors = require('cors');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const keys = require('./config/keys');
+const mongoose = require('mongoose');
+require('./models/User');
+require('./services/passport');
+
+
+//connect mongo to use mongoose
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+/* app middleware */
+//fix cors problem
 app.use(cors());
+//define usage of cookies
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000, //30 days expiried
+        keys: [keys.cookieKey]
+    })
+);
 
-app.get('/auth', (req, res) => {
-    res.send({ test: 'test'})
-})
+//tells app use cookies to manage auth
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(bodyParser.json()); //make POST req include the body to req obj
+/* app middleware*/
+
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000; // listen to Heroku's given port (prod), or take 5000 (dev)
 app.listen(PORT);
---
+++ services/passport.js
@@ -0,0 +1,38 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('../config/keys');
+const mongoose = require('mongoose');
+
+const User = mongoose.model('users'); //pull out 'users' collection
+
+//generate user to cookie
+passport.serializeUser((user, done) => {
+    done(null, user.id); //user.id in NOT profile.id, its the identifier mongo gives every record
+});
+
+//generate cookie to user
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+    .then(user => {
+        done(null, user);
+    })
+});
+
+//define authGoogle via passport API
+ passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback',
+    proxy: true
+},
+    async (accessToken, refreshToken, profile, done) => {
+        console.log(profile);
+        const existingUser = await User.findOne({ googleId: profile.id });
+        if (existingUser) {
+            //user already exists
+            return done(null, existingUser);
+        }
+            const user = await new User({ googleId: profile.id }).save();
+            done(null, user)
+    }
+));
